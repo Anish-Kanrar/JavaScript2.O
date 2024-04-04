8f13a7fd6ec9fcba7378c424368097dc9943162a
ANISH KANRAR <147317580+Anish-Kanrar@users.noreply.github.com>
2024-04-04 19:30:47 +0000

    comparison

---
+++ basic/06_Comparison.js
@@ -0,0 +1,47 @@
+/*
+console.log(3 > 1)   //true
+console.log(3 >= 1)  //true
+console.log(3 < 1)   //false
+console.log(3 == 1)  //false
+console.log(3 != 1)  //true
+
+console.log("3" > 1);   //true
+console.log("03" > 1);  //true
+
+console.log(null > 0)   //false
+console.log(null == 0)  //false
+console.log(null >= 0)  //true
+
+
+console.log(undefined == 0) //false
+console.log(undefined > 0)  //false
+console.log(undefined < 0)  //false
+*/
+
+//above all codes creats confusion, so avoid this type of comparison.
+
+//==
+console.log("3" == 3);  //true
+
+
+// ===
+console.log("3" === 3); //false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
