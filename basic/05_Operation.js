275e75e2a6ebf8a7adbb68cc752898d2e33920c2
ANISH KANRAR <147317580+Anish-Kanrar@users.noreply.github.com>
2024-04-04 16:05:41 +0000

    Operation

---
+++ basic/05_Operation.js
@@ -0,0 +1,24 @@
+let value = 18
+let negValue = -value
+//console.log(negValue) //-18
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3**4); //power
+// console.log(3/4);
+// console.log(3%4);  //modulus
+
+
+let str1 = "Hello"
+let str2 = " RCBians"
+
+let str3 = str1 + str2
+console.log(str3);  //Hello RCBians
+
+console.log(1 + 8)  //9
+console.log("1" + 8); //18
+console.log(1 + "8"); //18
+console.log("1" + 8 + 8); //188
+console.log(1 + 8 + "8");  //98
+console.log(1 + "8" + 1 + 2) //1812
