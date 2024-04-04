961458b2e43d0a09494dc6cbc582561c62aaad0b
ANISH KANRAR <147317580+Anish-Kanrar@users.noreply.github.com>
2024-04-04 13:10:36 +0000

    DataType

---
+++ basic/03_DataType.js
@@ -0,0 +1,39 @@
+"use strick";  //treat all JS code as newer version
+
+//JS er engineen browser er moddhe chupa hota hain
+//alert(7 + 7)  // we are using nodejs ,not browser 
+//alert("hello")
+
+
+//console.log(7 + 7); console.log("Anish") //not recommended,code readability should be high
+
+console.log(7+7)
+console.log("Anish")
+
+let name = "Anik"  //String
+let age = 20       //Number
+let isLoggedIn = false  //Boolean Type
+let state = null
+// Number => 2 to power 53
+// Bigint
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+// object
+
+
+console.log(typeof "Virat")   //string
+console.log(typeof age)       //number
+console.log(typeof null)      //object
+console.log(typeof undefined) //undefined
+
+
+/*Documentation
+1.mdn web docs
+2.tc39.es/ecma262
+*/
