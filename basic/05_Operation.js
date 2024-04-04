7d22c7f887cd3b0bae3ed99e9d3ed8de6912e2f8
ANISH KANRAR <147317580+Anish-Kanrar@users.noreply.github.com>
2024-04-04 17:58:56 +0000

    add op

--- basic/05_Operation.js
+++ basic/05_Operation.js
@@ -10,15 +10,35 @@ let negValue = -value
 // console.log(3%4);  //modulus
 
 
+/*
 let str1 = "Hello"
 let str2 = " RCBians"
 
 let str3 = str1 + str2
 console.log(str3);  //Hello RCBians
+*/
 
-console.log(1 + 8)  //9
-console.log("1" + 8); //18
-console.log(1 + "8"); //18
-console.log("1" + 8 + 8); //188
-console.log(1 + 8 + "8");  //98
-console.log(1 + "8" + 1 + 2) //1812
+// console.log(1 + 8)  //9
+// console.log("1" + 8); //18
+// console.log(1 + "8"); //18
+// console.log("1" + 8 + 8); //188
+// console.log(1 + 8 + "8");  //98
+// console.log(1 + "8" + 1 + 2) //1812
+
+//console.log((8 + 1)  * 5 % 2);   //code always simple,readable
+
+console.log(true);  //true
+console.log(+true);  //1
+console.log(false);  //false
+console.log(+false);  //0  //confussion code nehi likhna hai
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 500
+gameCounter++;
+console.log(gameCounter); //501
+
+//mdn,tc39.es/ecma262
+//increament op,decreament op
+//prefix,postfix
