3a583ef47ce7976dd241a00146c22f6edeb9c8a1
ANISH KANRAR <147317580+Anish-Kanrar@users.noreply.github.com>
2024-07-24 23:51:51 +0530

    Update generateOTP.js

--- 03_project/generateOTP.js
+++ 03_project/generateOTP.js
@@ -6,7 +6,7 @@ function generateOTP(length) {
     } return otp;
 }
 =======
-function generateOTP(length) {
+    function generateOTP(length) {
     let otp="";
     for(let i=0;i<length;i++){
         otp += Math.floor(Math.random() * 10).toString();
