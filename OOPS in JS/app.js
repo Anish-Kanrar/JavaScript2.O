fb2538c686626ff383175b8fb823414982f48fb9
ANISH KANRAR <147317580+Anish-Kanrar@users.noreply.github.com>
2024-07-25 01:13:22 +0530

    Add files via upload

---
+++ OOPS in JS/app.js
@@ -0,0 +1,64 @@
+// All power is within you;you can do anything and everything
+
+/** const student = {
+    fullName: "Anish Kanrar",
+    marks: 97.8,
+    printMarks: function () {
+        console.log("marks = ", this.marks);  //this--->student.marks
+    },
+};*/
+
+
+const employee = {
+    calcTax() {
+        console.log("tax rate is 10%")
+    },
+    // calcTax2 : function() {
+    //     console.log("tax rate is 10%")
+    // },
+};
+
+const anikDas = {
+    salary: 5000,
+    calcTax() {
+        console.log("tax rate is 20%");
+    },
+};
+
+anikDas.__proto__ = employee;
+
+// class is a program-code template for creating objects.
+
+class ToyotaCar{
+    constructor(){
+        console.log("creating new object");
+    }
+    start(){
+        console.log("Start")
+    }
+    stop(){
+        console.log("Stop")
+    }
+    setBrand(brand){
+        this.brand = brand;
+    }
+}
+
+let fortuner = new ToyotaCar("fortuner");  //constructor
+// fortuner.setBrand("fortuner");
+console.log(fortuner)
+let lexus = new ToyotaCar();  //constructor
+// lexus.setBrand("lexus");
+console.log(lexus)
+
+//inheritance is passing down properties & methods from parent class to chaild class
+
+class Parent{
+    hello(){
+        console.log("hello");
+    }
+}
+
+class child extends Parent{}
+
+let obj = new child();
---
+++ OOPS in JS/inheritence.js
@@ -0,0 +1,26 @@
+class person{
+    constructor(){
+        this.species = "homo sapiens"
+    }
+    eat(){
+        console.log("eat");
+    }
+    sleep(){
+        console.log("sleep");
+    }
+    work(){
+        console.log("do nothing");
+    }
+}
+
+class Enginner extends person{
+    work(){
+        console.log("solve problem, build something");
+    }
+}
+
+class Doctor extends person{
+    work(){
+        console.log("treat patients");
+    }
+}
