5a910160bd34a281bdec3ebc53ef090323503282
ANISH KANRAR <147317580+Anish-Kanrar@users.noreply.github.com>
2024-08-05 22:43:52 +0530

    Update errorHandiling.js

--- OOPS in JS/errorHandiling.js
+++ OOPS in JS/errorHandiling.js
@@ -3,10 +3,10 @@ let b = 3;
 console.log("a =",a);
 console.log("a =",b);
 console.log("a+b =",a+b);
-try{
-    
+try{   
 console.log("a+b =",a+c);  //error
-}catch(err){
+}
+catch(err){
     console.log(err);
 } //ReferenceError: c is not defined at errorHandiling.js:8:23
 console.log("a+b =",a+b);
