ecb7a95b6edf64b8d196d8b68f12dd13593b1b6a
AK47 <147317580+Anish-Kanrar@users.noreply.github.com>
2024-09-16 19:34:12 +0530

    Update 02_variables.js

--- 01_basic/02_variables.js
+++ 01_basic/02_variables.js
@@ -16,5 +16,6 @@ Prefer not to use var
 because of issue in block scope and functional scope   //{ }
 */
 
+
 //table method
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
