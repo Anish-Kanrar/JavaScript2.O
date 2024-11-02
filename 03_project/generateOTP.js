74c2eb9449197d2fb21c1a909ad24eeabdffb83e
AK47 <147317580+Anish-Kanrar@users.noreply.github.com>
2024-11-02 23:34:44 +0530

    Update generateOTP.js

--- 03_project/generateOTP.js
+++ 03_project/generateOTP.js
@@ -8,6 +8,7 @@ function generateOTP(length) {
 
 =======
     function generateOTP(length) {
+        
     let otp="";
     for(let i=0;i<length;i++){
         otp += Math.floor(Math.random() * 10).toString();
