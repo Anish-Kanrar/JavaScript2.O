39e78361f12791c50a6bff035a823cac7a684ddc
AK47 <147317580+Anish-Kanrar@users.noreply.github.com>
2024-11-13 23:45:10 +0530

    Update 02_arrays.js

--- 02_basic/02_arrays.js
+++ 02_basic/02_arrays.js
@@ -30,4 +30,5 @@ console.log(Array.from({name: "Anish"}));    //[], interesting
 let score1 = 100
 let score2 = 200
 let score3 = 300
+
 console.log(Array.of(score1, score2, score3));  //[ 100, 200, 300 ]
