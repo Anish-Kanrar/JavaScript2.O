34995e2929ee52ad868bef7d928d1eb741a60ae2
AK47 <147317580+Anish-Kanrar@users.noreply.github.com>
2024-11-14 23:10:36 +0530

    Add files via upload

---
+++ 03_project/From validation App/script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password")
+const Cpassword = document.getElementById("confirm-password")
+const submit = document.getElementById("btn")
