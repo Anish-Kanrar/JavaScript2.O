323203e485b135f1c1a19302d6cb83f134f773be
AK47 <147317580+Anish-Kanrar@users.noreply.github.com>
2024-12-05 23:54:29 +0530

    Update collegeWebsite.js

--- OOPS in JS/collegeWebsite.js
+++ OOPS in JS/collegeWebsite.js
@@ -24,6 +24,7 @@ let student1 = new User("Soham Mayur","cse22018@cemk.ac.in");
 let student2 = new User("Sagnik Metiya","cse22008@cemk.ac.in");
 let student3 = new User("Raja Das","cse22036@cemk.ac.in");
 
+
 let teacher1 = new User("Kazi Islam","kazi123@cemk.ac.in");
 
 let admin1 = new Admin("admin","admin@cemk.ac.in");
